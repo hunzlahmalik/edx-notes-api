988c7a95c2163dd78e614a0d99fa104ab21229d4
_ <50262751+hunzlahmalik@users.noreply.github.com>
2025-09-24 18:01:11 +0500

    feat: Upgrading storages with new dict Django52

--- notesserver/settings/yaml_config.py
+++ notesserver/settings/yaml_config.py
@@ -26,7 +26,27 @@ CONFIG_ROOT = Path(EDXNOTES_CONFIG_ROOT)
 with open(CONFIG_ROOT / "edx_notes_api.yml") as yaml_file:
     config_from_yaml = yaml.safe_load(yaml_file)
 
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
+
+default_file_storage = config_from_yaml.pop('DEFAULT_FILE_STORAGE', None)
+staticfiles_storage = config_from_yaml.pop('STATICFILES_STORAGE', None)
+
 vars().update(config_from_yaml)
+
+if default_file_storage:
+    STORAGES["default"]["BACKEND"] = default_file_storage
+if staticfiles_storage:
+    STORAGES["staticfiles"]["BACKEND"] = staticfiles_storage
+
 
 # Support environment overrides for migrations
 DB_OVERRIDES = {
